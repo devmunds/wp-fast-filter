c2d375b211743bae2c7919edf98ea99432701f6c
DEVMUNDS <devmunds@gmail.com>
2021-03-15 11:12:43 -0300

    Update: code/name

--- wp-fast-filter.php
+++ wp-fast-filter.php
@@ -1,7 +1,7 @@
 <?php
 /*
-* Plugin name: WP FAST FILTES
-* Version: 1.0.1
+* Plugin name: Wp Fast Filter
+* Version: 1.0.2
 * Description: Custom filter by shortcode [wp-fast-filtes posts_per_page="10" category_slug="economico"]
 * Author: Devmunds
 * Author uri: https://devmunds.com.br/
