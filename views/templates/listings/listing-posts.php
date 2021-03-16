fcea5cd62aef3a60e62154ba3adea1f62df3fee6
DEVMUNDS <devmunds@gmail.com>
2021-03-16 14:17:49 -0300

    Update: to version 1.0.5

--- views/templates/listings/listing-posts.php
+++ views/templates/listings/listing-posts.php
@@ -1,4 +1,5 @@
 <article class="filter-post-wrap">                            
     <h2 class="entry-title"><a href="' <?php echo  get_post_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
-    <?php the_excerpt(); ?>                       
+    <?php the_excerpt(); ?> 
+    <div class="align-right"><span>Publicado: <?php echo get_the_date(); ?></span></div>                      
 </article>
